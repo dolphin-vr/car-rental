71b329faac19fa6b2c2d65a38aa692ced1487233
dolphin <fun53@ukr.net>
2024-01-21 22:05:51 +0200

    styling

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/car-rental",
-})
+   plugins: [react()],
+   base: "/car-rental",
+});
