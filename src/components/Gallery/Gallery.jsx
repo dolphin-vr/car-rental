bdede1529c93a7b388f341a426179348c9c98eca
dolphin <fun53@ukr.net>
2024-01-22 03:16:25 +0200

    http-fix

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,15 +1,10 @@
-import { useSelector } from "react-redux";
-import { selectIsLoading } from "../../redux/carsSlice";
 import { List } from "./Gallery.styled";
 import { Card } from "../Card/Card";
-import { Loader } from "../Loader/Loader";
 
 export const Gallery = ({list, onClick}) => {
-	const isLoading = useSelector(selectIsLoading);
 	return (
 		<List>
-			{isLoading && <Loader />}
-			{!isLoading && list.map(el => <Card key={el.id} car={el} onClick={onClick} />)}
+			{list.map(el => <Card key={el.id} car={el} onClick={onClick} />)}
 		</List>
 	)
 }
